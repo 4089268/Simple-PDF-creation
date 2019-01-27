903937a0ccba0f4a37c2b4cd66c3288924c63f9e
Salvador <chava_r17@hotmail.com>
2019-01-26 19:40:47 -0600

    Generacion del Proyecto
    Se genero y agrego el codigo del proyecto

---
+++ Create_PDF_VB/Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Broswa = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Broswa)
+        Me.GroupBox1.Controls.Add(Me.DataGridView1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(723, 347)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Exportar a PDF"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Create_PDF_VB.My.Resources.Resources.logo
+        Me.PictureBox1.Location = New System.Drawing.Point(7, 288)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(144, 53)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(276, 314)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(389, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "GENERAR PDF"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(157, 314)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(113, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "RUTA"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Broswa
+        '
+        Me.Broswa.AutoSize = True
+        Me.Broswa.Location = New System.Drawing.Point(100, 291)
+        Me.Broswa.Name = "Broswa"
+        Me.Broswa.Size = New System.Drawing.Size(33, 13)
+        Me.Broswa.TabIndex = 2
+        Me.Broswa.Text = "Ruta:"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 45)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(701, 236)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(157, 288)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(508, 20)
+        Me.TextBox2.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(103, 19)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(604, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(61, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Titulo:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(770, 371)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Generar Pdf"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Broswa As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Create_PDF_VB/Form1.vb
@@ -0,0 +1,115 @@
+﻿Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+Imports System.IO
+
+
+Public Class Form1
+    Dim dt As DataTable
+    Public Sub New()
+        InitializeComponent()
+        dt = New DataTable
+        dt.Columns.Add("Dosage", GetType(Integer))
+        dt.Columns.Add("Drug", GetType(String))
+        dt.Columns.Add("Patient", GetType(String))
+        dt.Columns.Add("Date", GetType(DateTime))
+
+        dt.Rows.Add(25, "Indocin", "David", DateTime.Now)
+        dt.Rows.Add(50, "Enebrel", "Sam", DateTime.Now)
+        dt.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now)
+        dt.Rows.Add(21, "Combivent", "Janet", DateTime.Now)
+        dt.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now)
+
+        DataGridView1.DataSource = dt
+
+        SaveFileDialog1.FileName = ""
+        SaveFileDialog1.Filter = "PDF (*.pdf)|*.pdf"
+
+        TextBox1.Text = "INGRESOS POR DIAS DEL 10/10/2017 AL 30/11/2017"
+        TextBox2.Text = "Seleccione una rutaa..."
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SaveFileDialog1.FileName = ""
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            TextBox2.Text = SaveFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim paragraph As Paragraph
+        Dim PdfFile As New Document(PageSize.LETTER, 40, 40, 2, 20)
+        PdfFile.AddTitle(TextBox1.Text)
+        Dim writer As PdfWriter = PdfWriter.GetInstance(PdfFile, New FileStream(TextBox2.Text, FileMode.Create))
+        PdfFile.Open()
+
+        'Declarasion de Estilos
+        Dim pTitle As New Font(iTextSharp.text.Font.FontFamily.HELVETICA, 14, iTextSharp.text.Font.BOLD, BaseColor.BLACK)
+        Dim pTable As New Font(iTextSharp.text.Font.FontFamily.HELVETICA, 12, iTextSharp.text.Font.NORMAL, BaseColor.BLACK)
+
+        'Imagen Cabecera        
+        Dim converter As New ImageConverter
+        Dim headerImg As iTextSharp.text.Image = iTextSharp.text.Image.GetInstance(converter.ConvertTo(Global.Create_PDF_VB.My.Resources.Resources.logo, GetType(Byte())))
+        Dim head As New PdfPTable(1)
+        Dim cimg As New PdfPCell(headerImg, True)
+        cimg.HorizontalAlignment = Element.ALIGN_CENTER
+        cimg.FixedHeight = 60.0F
+        cimg.Border = PdfPCell.NO_BORDER
+        head.AddCell(cimg)
+        PdfFile.Add(head)
+
+
+        'Agregar Titulo al PDf
+        paragraph = New Paragraph(New Chunk(TextBox1.Text, pTitle))
+        paragraph.Alignment = Element.ALIGN_CENTER
+        paragraph.SpacingAfter = 5.0F
+        paragraph.SpacingBefore = 5.0F
+        PdfFile.Add(paragraph)
+
+
+        'Insertar datos en la tabla
+        Dim PdfTable As New PdfPTable(DataGridView1.Columns.Count)
+
+        'COnfigurara Tabla
+        PdfTable.TotalWidth = 500.0F
+        PdfTable.LockedWidth = True
+
+        Dim widths(0 To DataGridView1.Columns.Count - 1) As Single
+
+        For i As Integer = 0 To DataGridView1.Columns.Count - 1
+            widths(i) = 1.0F
+        Next
+
+        PdfTable.SetWidths(widths)
+        PdfTable.HorizontalAlignment = 0
+        PdfTable.SpacingBefore = 5.0F
+
+        'Declaracion de las celdas 
+        Dim pdfcell As New PdfPCell
+        For i As Integer = 0 To DataGridView1.Columns.Count - 1
+
+            'cabecera de la celda
+            pdfcell = New PdfPCell(New Phrase(New Chunk(DataGridView1.Columns(i).HeaderText, pTable)))
+            pdfcell.HorizontalAlignment = PdfPCell.ALIGN_LEFT
+
+            PdfTable.AddCell(pdfcell)
+
+        Next
+
+        For i As Integer = 0 To DataGridView1.Rows.Count - 2
+            For j As Integer = 0 To DataGridView1.Columns.Count - 1
+                pdfcell = New PdfPCell(New Phrase(DataGridView1(j, i).Value.ToString(), pTable))
+                PdfTable.HorizontalAlignment = PdfPCell.ALIGN_LEFT
+                PdfTable.AddCell(pdfcell)
+            Next
+        Next
+
+        PdfFile.Add(PdfTable)
+        PdfFile.Close()
+        MsgBox("Pdf Creado", MsgBoxStyle.OkOnly)
+
+    End Sub
+
+
+End Class
